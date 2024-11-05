e8e42ab7bfc7cbbc642603e277f9617a15969b2d
William Noronha Mattede <32982645+dablho@users.noreply.github.com>
2024-11-04 23:11:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from collections import Counter
 import networkx as nx
 import os
 
-nltk.download('all')
 # Definir o diretório para armazenar os dados do NLTK
 nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
 if not os.path.exists(nltk_data_dir):
@@ -21,11 +20,7 @@ if not os.path.exists(nltk_data_dir):
 
 # Adicionar o diretório ao caminho de dados do NLTK
 nltk.data.path.append(nltk_data_dir)
-
-# Baixar os pacotes necessários do NLTK para esse diretório
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('stopwords', download_dir=nltk_data_dir)
-nltk.download('wordnet', download_dir=nltk_data_dir)
+nltk.download('all', download_dir=nltk_data_dir)
 
 st.title("Análise de Sentimentos dos Comentários Negativos de Instituições Financeiras Brasileiras!")
 
@@ -49,21 +44,15 @@ def remove_accent(text):
     return ''.join((c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn'))
 
 def preprocess_text(text, remove_stopwords=True):
-    # Remove acentos
     text = remove_accent(text)
-    # Converter para minúsculas
     text = text.lower()
-    # Remove pontuação
     text = text.translate(str.maketrans('', '', string.punctuation))
-    # Remove números
     text = ''.join([i for i in text if not i.isdigit()])
-    # Tokenização
     words = word_tokenize(text)
     if remove_stopwords:
-        # Remover stopwords
         stop_words = set(stopwords.words('portuguese'))
         words = [word for word in words if word not in stop_words]
-    # Lematização
+        
     lemmatizer = WordNetLemmatizer()
     words = [lemmatizer.lemmatize(word) for word in words]
     return words
@@ -90,22 +79,18 @@ def plot_ngram_bar_chart(ngrams, title):
     ax.barh(ngrams, counts, color="skyblue")
     ax.set_title(title)
     ax.set_xlabel('Frequência')
-    plt.gca().invert_yaxis()  # Inverter o eixo y para colocar a maior barra no topo
+    plt.gca().invert_yaxis()
     st.pyplot(fig)
 
-# Obter dados do banco de dados
 data = get_data_from_db()
 
 if data is not None:
-    # Mostrar os dados no Streamlit
     st.write('Aqui estão os dados extraídos do banco de dados:')
     st.write(data)
 
-    # Selecionar o número de palavras mais frequentes
     num_words = st.selectbox('Selecione o número de palavras mais frequentes para exibir nos gráficos de barras:', [10, 20, 30])
 
-    # Nuvem de palavras com stopwords
-    st.write('Nuvem de palavras dos comentários (com stopwords):')
+    st.write('Nuvem de palavras dos comentários (sem stopwords):')
     text_with_stopwords = " ".join(comment for comment in data['comentario'])
     wordcloud_with_stopwords = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text_with_stopwords)
 
@@ -114,8 +99,7 @@ if data is not None:
     ax_with_stopwords.axis("off")
     st.pyplot(fig_with_stopwords)
 
-    # Gráfico de barras para palavras mais frequentes com stopwords
-    st.write(f'Gráfico de barras das {num_words} palavras mais frequentes (com stopwords):')
+    st.write(f'Gráfico de barras das {num_words} palavras mais frequentes (sem stopwords):')
     words_with_stopwords = preprocess_text(text_with_stopwords, remove_stopwords=False)
     word_freq_with_stopwords = Counter(words_with_stopwords)
     most_common_with_stopwords = word_freq_with_stopwords.most_common(num_words)
@@ -126,14 +110,12 @@ if data is not None:
     ax_bar_with_stopwords.set_title(f'{num_words} palavras mais frequentes (com stopwords)')
     ax_bar_with_stopwords.set_xlabel('Palavras')
     ax_bar_with_stopwords.set_ylabel('Frequência')
-    ax_bar_with_stopwords.tick_params(axis='x', rotation=45)  # Rotaciona os rótulos para 45 graus
+    ax_bar_with_stopwords.tick_params(axis='x', rotation=45)
     st.pyplot(fig_bar_with_stopwords)
 
-    # Preprocessar os comentários para remover stopwords
     data['comentario'] = data['comentario'].apply(lambda x: " ".join(preprocess_text(x, remove_stopwords=True)))
 
-    # Nuvem de palavras sem stopwords
-    st.write('Nuvem de palavras dos comentários (sem stopwords):')
+    st.write('Nuvem de palavras dos comentários (com stopwords):')
     text_without_stopwords = " ".join(comment for comment in data['comentario'])
     wordcloud_without_stopwords = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text_without_stopwords)
 
@@ -142,9 +124,7 @@ if data is not None:
     ax_without_stopwords.axis("off")
     st.pyplot(fig_without_stopwords)
 
-
-    # Gráfico de barras para palavras mais frequentes sem stopwords
-    st.write(f'Gráfico de barras das {num_words} palavras mais frequentes (sem stopwords):')
+    st.write(f'Gráfico de barras das {num_words} palavras mais frequentes (com stopwords):')
     words_without_stopwords = preprocess_text(text_without_stopwords, remove_stopwords=False)
     word_freq_without_stopwords = Counter(words_without_stopwords)
     most_common_without_stopwords = word_freq_without_stopwords.most_common(num_words)
@@ -155,35 +135,67 @@ if data is not None:
     ax_bar_without_stopwords.set_title(f'{num_words} palavras mais frequentes (sem stopwords)')
     ax_bar_without_stopwords.set_xlabel('Palavras')
     ax_bar_without_stopwords.set_ylabel('Frequência')
-    ax_bar_without_stopwords.tick_params(axis='x', rotation=45)  # Rotaciona os rótulos para 45 graus
+    ax_bar_without_stopwords.tick_params(axis='x', rotation=45)
     st.pyplot(fig_bar_without_stopwords)
 
-    # Criar grafo de tetragramas
     st.write(f'Grafo dos tetragramas mais frequentes e suas conexões:')
-    G, most_common_ngrams = create_ngram_graph(text_without_stopwords, n=4, top_n=10)  # Usar 10 tetragramas mais frequentes
+    G, most_common_ngrams = create_ngram_graph(text_without_stopwords, n=4, top_n=10)
 
-    # Definir a seed para o layout do grafo
-    pos = nx.spring_layout(G, k=3, seed=20, iterations=50)  # Ajuste o valor de k para controlar o espaçamento e aumente as iterações para melhor convergência
-
+    pos = nx.spring_layout(G, k=3, seed=20, iterations=50)
     fig_graph, ax_graph = plt.subplots(figsize=(12, 8))
 
-    # Desenhar os nós com transparência
     nx.draw_networkx_nodes(G, pos, node_size=1500, node_color="skyblue", alpha=0.5, ax=ax_graph)
-
-    # Desenhar as arestas com setas
     nx.draw_networkx_edges(G, pos, ax=ax_graph, arrowstyle='-|>', arrowsize=20)
-
-    # Desenhar os rótulos dos nós
     labels = {node: node for node in G.nodes()}
     nx.draw_networkx_labels(G, pos, labels=labels, font_size=10, font_color="black", ax=ax_graph)
 
     plt.title('Grafo dos tetragramas mais frequentes e suas conexões')
     st.pyplot(fig_graph)
 
-    # Obter os 20 tetragramas mais frequentes para o gráfico de barras
     _, most_common_20_ngrams = create_ngram_graph(text_without_stopwords, n=4, top_n=20)
-    
-    # Gráfico de barras dos 20 tetragramas mais frequentes
     plot_ngram_bar_chart(most_common_20_ngrams, 'Gráfico de barras dos 20 tetragramas mais frequentes')
 else:
     st.write('Não foi possível carregar os dados.')
+
+# Parte 2
+st.subheader("Parte 2")
+
+# Lista de stopwords personalizadas para serem adicionadas às stopwords do NLTK
+additional_stopwords = {"para", "un", "exemplo", "ser", "poder", "so"}
+
+# Define as stopwords incluindo as adicionais
+stop_words = set(stopwords.words('portuguese')).union(additional_stopwords)
+
+# Processa cada comentário individualmente e junta as palavras em uma string para a nuvem de palavras
+text_with_stopwords = " ".join(" ".join(preprocess_text(comment, remove_stopwords=True)) for comment in data['comentario'])
+
+# Nuvem de palavras com stopwords (incluindo stopwords adicionais) e preprocessamento
+st.write('Nuvem de palavras dos comentários (com stopwords):')
+wordcloud_with_stopwords = WordCloud(stopwords=stop_words, max_font_size=50, max_words=100, background_color="white").generate(text_with_stopwords)
+
+fig_with_stopwords, ax_with_stopwords = plt.subplots(figsize=(10, 6))
+ax_with_stopwords.imshow(wordcloud_with_stopwords, interpolation='bilinear')
+ax_with_stopwords.axis("off")
+st.pyplot(fig_with_stopwords)
+
+# Gráfico de barras para palavras mais frequentes com stopwords personalizadas
+st.write(f'Gráfico de barras das {num_words} palavras mais frequentes (com stopwords personalizadas):')
+
+# Pré-processa o texto para o gráfico de barras, removendo as stopwords personalizadas
+words_with_stopwords = [
+    word for comment in data['comentario']
+    for word in preprocess_text(comment, remove_stopwords=True)
+    if word not in stop_words
+]
+
+word_freq_with_stopwords = Counter(words_with_stopwords)
+most_common_with_stopwords = word_freq_with_stopwords.most_common(num_words)
+words, counts = zip(*most_common_with_stopwords)
+
+fig_bar_with_stopwords, ax_bar_with_stopwords = plt.subplots(figsize=(10, 6))
+ax_bar_with_stopwords.bar(words, counts)
+ax_bar_with_stopwords.set_title(f'{num_words} palavras mais frequentes (com stopwords personalizadas)')
+ax_bar_with_stopwords.set_xlabel('Palavras')
+ax_bar_with_stopwords.set_ylabel('Frequência')
+ax_bar_with_stopwords.tick_params(axis='x', rotation=45)
+st.pyplot(fig_bar_with_stopwords)
